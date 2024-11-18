c1bda6a3d326a971f0c5bc109f5df160b31f506a
ben256dev <benjamin@ben256.com>
2024-11-18 15:46:28 -0700

    xmalloc

--- butil.h
+++ butil.h
@@ -13,3 +13,11 @@
         fprintf(stderr, "%s:%d \x1b[1;31merror: %s(): " fmt ": \x1b[0m%s\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__, strerror(errno)); \
         exit(EXIT_FAILURE); \
     } while (0)
+
+void* xmalloc(size_t size)
+{
+   void* ptr = malloc(size);
+   if (ptr == NULL)
+      pdie("malloc(%zu)", size);
+   return ptr;
+}
