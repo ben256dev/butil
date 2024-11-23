4b4c4ec8f69e4335b68f69f0b82bd89446f82a05
ben256dev <benjamin@ben256.com>
2024-11-22 21:30:45 -0700

    xfopen

--- butil.h
+++ butil.h
@@ -1,11 +1,13 @@
 #pragma once
 
+#include <limits.h>
+#include <stdio.h>
 #include <errno.h>
 
 #define die(fmt, ...) \
     do { \
         fflush(stdout); \
-        fprintf(stderr, "%s:%d \x1b[1;31merror: %s()\x1b[0m " fmt "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__); \
+        fprintf(stderr, "%s:%d \x1b[1;31merror: %s(): \x1b[0m" fmt "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__); \
         exit(-1); \
     } while (0)
 
@@ -22,4 +24,11 @@ void* xmalloc(size_t size)
    if (ptr == NULL)
       pdie("malloc(%zu)", size);
    return ptr;
+}
+FILE* xfopen(const char* file_path, const char* mode)
+{
+   FILE* fd = fopen(file_path, mode);
+   if (fd == NULL)
+      pdie("fopen(%s, %s)", file_path, mode);
+   return fd;
 }
